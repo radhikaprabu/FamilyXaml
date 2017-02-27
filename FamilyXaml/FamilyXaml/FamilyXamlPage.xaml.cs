8a72e374258eeb6f7b9ce838ac2b90d350214f64
radhikaprabu <radhikaprabu91699@gmail.com>
2017-02-27 12:23:06 -0600

    Add project files.

---
+++ FamilyXaml/FamilyXaml/FamilyXamlPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace FamilyXaml
+{
+    public partial class FamilyXamlPage : ContentPage
+    {
+        public FamilyXamlPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
